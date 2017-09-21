21da8efd9a22af2be93f883650dc246552bcc9ed
Quincy <jayever@163.com>
2017-09-21 10:23:28 +0800

    add pm2.json

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = [
     {
         host: 'www.test.com',
+        proxy: '127.0.0.1:8080'
+    },
+    {
+        host: 'm.test.com',
         proxy: '127.0.0.1:8001'
     },
     {
